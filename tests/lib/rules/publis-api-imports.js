567cea1dd0362bc94e8df6b5728469c232fb9ccb
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-10-23 11:02:40 +0500

    upd версии(добавлена логика проверки тестовых импортов)

--- tests/lib/rules/publis-api-imports.js
+++ tests/lib/rules/publis-api-imports.js
@@ -8,7 +8,7 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/publis-api-imports"),
+const rule = require("../../../lib/rules/public-api-imports"),
   RuleTester = require("eslint").RuleTester;
 
 
@@ -46,15 +46,15 @@ ruleTester.run("public-api-imports", rule, {
                 testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
             }],
         },
-        // {
-        //     filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\StoreDecorator.tsx',
-        //     code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
-        //     errors: [],
-        //     options: [{
-        //         alias: '@',
-        //         testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
-        //     }],
-        // }
+        {
+            filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\StoreDecorator.tsx',
+            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+            errors: [],
+            options: [{
+                alias: '@',
+                testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
+            }],
+        }
     ],
 
     invalid: [
@@ -71,20 +71,20 @@ ruleTester.run("public-api-imports", rule, {
         {
             filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\StoreDecorator.tsx',
             code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing/file.tsx'",
-            errors: [{message: 'Абсолютный импорт разрешен только из Public API (index.ts)'}],
+            errors: [{message: 'Тестовые данные необходимо импортировать из publicApi/testing.ts'}],
             options: [{
                 alias: '@',
                 testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
             }],
         },
-        // {
-        //     filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\forbidden.ts',
-        //     code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
-        //     errors: [{message: 'Тестовые данные необходимо импортировать из publicApi/testing.ts'}],
-        //     options: [{
-        //         alias: '@',
-        //         testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
-        //     }],
-        // }
+        {
+            filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\forbidden.ts',
+            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+            errors: [{ message: "Абсолютный импорт разрешен только из Public API (index.ts)"}],
+            options: [{
+                alias: '@',
+                testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
+            }],
+        }
     ],
 });
